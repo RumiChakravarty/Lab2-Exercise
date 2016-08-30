c34837c8235d6c0eb980e6c7b03549f9b5bc98aa
RumiChakravarty <rumi.sarma.c@gmail.com>
2016-08-30 16:54:45 -0400

    Lab 2_.Net
    Lab 2_.Net
    Submitted by Rumi

--- Lab2 Exercise/Program.cs
+++ Lab2 Exercise/Program.cs
@@ -13,8 +13,10 @@ namespace Lab2_Exercise
             string name = "player";
 
 
-            Console.WriteLine("Welcome! What is your name?");
-            string username = Console.ReadLine();
+            // Console.WriteLine("Welcome! What is your name?");
+            //tring username = Console.ReadLine();
+            //UserName();
+            string user = UserName();
             Console.WriteLine("\nWould you like to play a game? ( \"yes\" or \"no\") :");
             string playgame = Console.ReadLine();
             
@@ -30,50 +32,52 @@ namespace Lab2_Exercise
                     string head = Console.ReadLine();
                     switch(head)
                     {
-                        case "1":
-                            Console.WriteLine($"No one has ever faced {head} dragon befor! ");
-                            break;
-                        case "2":
-                            Console.WriteLine($"No one has ever faced a {head} dragon befor! ");
-                            break;
+                        case "1":                           
+                        case "2":                            
                         case "3":
                             Console.WriteLine($"No one has ever faced a {head}- headed dragon befor!");
-                            break;
+                            Console.WriteLine("Choose your weapon.  \"slingshot\" or \"sword\"or \"bow and arrow\"");
 
-                    }
-                    Console.WriteLine("Choose your weapon.  \"slingshot\" or \"sword\"or \"bow and arrow\"");
-
-                    string weapon = Console.ReadLine();
-                    switch(weapon)
-                    {
-                        case "slingshot":
-                            Console.WriteLine("Armed with your Slingshot");
-                            break;
-                        case "sword":
-                            Console.WriteLine("Armed with your Sword");
-                            break;
-                        case "bow and arrow":
-                            Console.WriteLine("Armed with your bow and arrow, you approach the dragon. You can feel it fiery breath as you get closer.\nIt stares at you with its _ eyes.(\"red\" or \"blue\")");
-
-                            string eye = Console.ReadLine();
-                            switch (eye)
-                                {
-                                case "red":
-                                    Console.WriteLine("Oh thank goodness! Red eyed dragons are friendly. You pet it and become friends.\n You name the dragon_______. (Enter a name)");
-                                     name = Console.ReadLine();
-                                    Console.WriteLine(username +" and " + name +" live happily ever after");
+                            string weapon = Console.ReadLine();
+                            switch (weapon)
+                            {
+                                case "slingshot":
+                                    Console.WriteLine("Armed with your Slingshot");
                                     break;
-                        case "blue":
-                                    Console.WriteLine("Oh thank goodness! Blue eyed dragons are dangerous.");
+                                case "sword":
+                                    Console.WriteLine("Armed with your Sword");
+                                    break;
+                                case "bow and arrow":
+                                    Console.WriteLine("Armed with your bow and arrow, you approach the dragon. You can feel it fiery breath as you get closer.\nIt stares at you with its _ eyes.(\"red\" or \"blue\")");
+
+                                    string eye = Console.ReadLine();
+                                    switch (eye)
+                                    {
+                                        case "red":
+                                            Console.WriteLine("Oh thank goodness! Red eyed dragons are friendly. You pet it and become friends.\n You name the dragon_______. (Enter a name)");
+                                            name = Console.ReadLine();
+                                            Console.WriteLine(user + " and " + name + " live happily ever after");
+                                            Console.Read();
+                                            break;
+                                        case "blue":
+                                            Console.WriteLine("Oh thank goodness! Blue eyed dragons are dangerous.");
+
+                                            break;
+
+
+                                    }
 
                                     break;
-
 
                             }
-
+                            break;
+                        default:
+                            Console.WriteLine("End of the Game");
                             break;
 
+
                     }
+                   
 
 
                 }
@@ -89,5 +93,13 @@ namespace Lab2_Exercise
 
 
         }
+        static string UserName()
+        {
+            Console.WriteLine("Welcome! What is your name?");
+            string username = Console.ReadLine();
+            return username;
+        }
     }
 }
+
+
